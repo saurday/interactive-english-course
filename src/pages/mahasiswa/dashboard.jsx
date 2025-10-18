ec93e9bbde4ff458da5da025e3c5f63a9a002cf4
Nisaur Rohmah <113544206+saurday@users.noreply.github.com>
2025-10-18 14:39:45 +0700

    Change heading from h4 to h3 for level display

--- src/pages/mahasiswa/dashboard.jsx
+++ src/pages/mahasiswa/dashboard.jsx
@@ -522,7 +522,7 @@ button.menu-item{
               </>
             ) : (
               <>
-                <h4>
+                <h3>
                   Your level:&nbsp;
                   <b>
                     {placement.latest.level}
@@ -532,7 +532,7 @@ button.menu-item{
                           .trim()})`
                       : ""}
                   </b>
-                </h4>
+                </h3>
 
                 <p style={{ marginBottom: 12 }}>
                   Score {placement.latest.correct ?? 0}/
